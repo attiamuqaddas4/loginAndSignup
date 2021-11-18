ad60a0ccb609c0a6128a4855f294aaa05263a0d7
Attia Muqaddas <attia.muqaddas4@gmail.com>
2021-11-18 20:57:39 +0500

    login and sign up activities

---
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+        private TextView txtSignUp;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+
+            txtSignUp = findViewById(R.id.txtSignUp);
+            txtSignUp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+        }
+}
---
+++ app/src/main/java/com/example/login/SignUpActivity.java
@@ -0,0 +1,30 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+    private TextView tvLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        tvLogin = findViewById(R.id.textLogin);
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
